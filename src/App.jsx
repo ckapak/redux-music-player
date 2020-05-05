079799a19d39c97e91591054ffe91289cd0b8727
ckapak <christinekapak@googlemail.com>
2020-05-05 12:12:55 +0100

    app component added and styling applied to the body

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
+import { FormGroup, FormControl, InputGroup } from 'react-bootstrap'
 
 class App extends Component {
   render() {
@@ -8,6 +8,15 @@ class App extends Component {
       <div className="App">
         <div>
           <div className=".App-title">Music Player from App</div>
+          <FormGroup>
+            <InputGroup>
+              <FormControl
+                className="seachbar"
+                type="text"
+                placeholder="Search for an artist"
+              />
+            </InputGroup>
+          </FormGroup>
           <div className="Profile">
             <div>Artist Picture</div>
             <div>Artist Name</div>
